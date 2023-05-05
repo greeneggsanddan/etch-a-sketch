ef87c17fec46e4603038702ee83bf58af8ca8020
greeneggsanddan <greeneggsanddan@gmail.com>
2023-05-05 16:40:22 -0700

    Add a grid to the page

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,9 +1,20 @@
 const container = document.querySelector("#container");
 
 function createGrid (size) {
-    for (let i = 0; i < size * size; i++) {
-        const div = document.createElement("div");
-        div.classList.add("grid");
-        container.appendChild(div);
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < size; j++) {
+            const div = document.createElement("div");
+            if (i == size -1) {
+                div.classList.add("grid-bottom");
+            } else {
+                div.classList.add("grid");
+            }
+            row.appendChild(div);
+        }
+        container.appendChild(row);
     }
 }
+
+createGrid(10);
