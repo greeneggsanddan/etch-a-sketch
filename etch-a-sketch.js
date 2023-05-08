3918c1a869f8465bacc5ac7e42fa90214c19d6c9
greeneggsanddan <greeneggsanddan@gmail.com>
2023-05-07 22:02:54 -0700

    Fix removeGrid functionality

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,6 +6,9 @@ function createGrid (size) {
         row.classList.add("row");
         for (let j = 0; j < size; j++) {
             const div = document.createElement("div");
+            div.addEventListener("mouseover", () => {
+                div.style.backgroundColor = "black";
+            });
             if (i == size -1) {
                 div.classList.add("grid-bottom");
             } else {
@@ -16,5 +19,26 @@ function createGrid (size) {
         container.appendChild(row);
     }
 }
+
+function removeGrid() {
+}
+
+const smallButton = document.querySelector("#small");
+smallButton.addEventListener("click", () => {
+    document.getElementById("container").innerHTML = "";
+    createGrid(10);
+});
+
+const mediumButton = document.querySelector("#medium");
+mediumButton.addEventListener("click", () => {
+    document.getElementById("container").innerHTML = "";
+    createGrid(50);
+});
+
+const largeButton = document.querySelector("#large");
+largeButton.addEventListener("click", () => {
+    document.getElementById("container").innerHTML = "";
+    createGrid(100);
+});
 
 createGrid(10);
